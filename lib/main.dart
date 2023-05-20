d162c30f24e5459f1ef5302866a5576a51cd7cdf
Valeron <valerontoscano@gmail.com>
2023-05-20 17:51:34 +0530

    Removed sidebarx dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:malhar_2023/pages/contact.dart';
-import 'package:sidebarx/sidebarx.dart';
 import 'package:get/get.dart';
 
 import 'components/drawer.dart';
@@ -35,8 +33,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final _controller = SidebarXController(selectedIndex: 0, extended: true);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -49,7 +45,6 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Colors.transparent,
         elevation: 0,
       ),
-      // drawer: ExampleSidebarX(controller: _controller),
       drawer: const CustomDrawer(),
       body: Container(
         decoration: const BoxDecoration(
@@ -65,126 +60,7 @@ class _MyHomePageState extends State<MyHomePage> {
             ],
           ),
         ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
-
-class ExampleSidebarX extends StatelessWidget {
-  ExampleSidebarX({
-    Key? key,
-    required SidebarXController controller,
-  })  : _controller = controller,
-        super(key: key);
-
-  final SidebarXController _controller;
-
-  final height = AppBar().preferredSize.height;
-
-  @override
-  Widget build(BuildContext context) {
-    var statusbarheight = MediaQuery.of(context).viewPadding.top;
-
-    return SidebarX(
-      controller: _controller,
-      theme: SidebarXTheme(
-        margin: EdgeInsets.fromLTRB(10, height + statusbarheight, 0, 10),
-        decoration: BoxDecoration(
-          gradient: const LinearGradient(colors: [
-            Color.fromRGBO(169, 171, 220, 1),
-            Color.fromRGBO(212, 191, 212, 1)
-          ], transform: GradientRotation(2.7)),
-          borderRadius: BorderRadius.circular(20),
-        ),
-        hoverColor: scaffoldBackgroundColor,
-        textStyle: TextStyle(color: Colors.white.withOpacity(0.7)),
-        selectedTextStyle: const TextStyle(color: Colors.white),
-        itemTextPadding: const EdgeInsets.only(left: 30),
-        itemPadding: const EdgeInsets.symmetric(vertical: 15),
-        selectedItemMargin: const EdgeInsets.symmetric(horizontal: 5),
-        itemMargin: const EdgeInsets.symmetric(horizontal: 5),
-        selectedItemPadding: const EdgeInsets.symmetric(vertical: 15),
-        selectedItemTextPadding: const EdgeInsets.only(left: 30),
-        itemDecoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          border: Border.all(color: const Color.fromARGB(255, 38, 5, 44)),
-          gradient: const LinearGradient(
-            colors: [
-              Color.fromARGB(255, 118, 102, 155),
-              Color.fromARGB(255, 101, 78, 121)
-            ],
-          ),
-        ),
-        selectedItemDecoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          border: Border.all(
-            color: actionColor.withOpacity(0.37),
-          ),
-          gradient: const LinearGradient(
-            colors: [accentCanvasColor, canvasColor],
-          ),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.28),
-              blurRadius: 30,
-            )
-          ],
-        ),
-        iconTheme: IconThemeData(
-          color: Colors.white.withOpacity(0.7),
-          size: 20,
-        ),
-        selectedIconTheme: const IconThemeData(
-          color: Colors.white,
-          size: 20,
-        ),
-      ),
-      extendedTheme: const SidebarXTheme(
-        margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
-        padding: EdgeInsets.all(5),
-        itemPadding: EdgeInsets.all(15),
-        selectedItemPadding: EdgeInsets.all(15),
-        itemMargin: EdgeInsets.only(left: 10),
-        selectedItemMargin: EdgeInsets.only(left: 10),
-        width: 200,
-        decoration: BoxDecoration(
-          color: canvasColor,
-        ),
-      ),
-      footerDivider: divider,
-      items: [
-        SidebarXItem(
-          icon: Icons.home,
-          label: 'Home',
-          onTap: () {
-            debugPrint('Home');
-          },
-        ),
-        const SidebarXItem(
-          icon: Icons.search,
-          label: 'Search',
-        ),
-        const SidebarXItem(
-          icon: Icons.people,
-          label: 'People',
-        ),
-        const SidebarXItem(
-          icon: Icons.favorite,
-          label: 'Favorites',
-        ),
-        const SidebarXItem(
-          iconWidget: FlutterLogo(size: 20),
-          label: 'Flutter',
-        ),
-      ],
-    );
-  }
-}
-
-const primaryColor = Color(0xFF685BFF);
-const canvasColor = Color(0xFF2E2E48);
-const scaffoldBackgroundColor = Color(0xFF464667);
-const accentCanvasColor = Color.fromRGBO(29, 47, 81, 1);
-const white = Colors.white;
-final actionColor = const Color(0xFF5F5FA7).withOpacity(0.6);
-final divider = Divider(color: white.withOpacity(0.3), height: 1);
