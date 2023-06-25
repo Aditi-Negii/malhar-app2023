9c0e8fd35e3e32a352c6f3a196e8d510b95cacbd
Valeron <valerontoscano@gmail.com>
2023-06-25 20:26:15 +0530

    Fixed notifications

--- lib/data/notfifications.dart
+++ lib/data/notfifications.dart
@@ -1,12 +1,44 @@
 var notificationData = [
     {
-      'Title': 'Event1',
-      'Desc': 'Some description',
-      'Time': 1
+      'Title': 'Test',
+      'Desc': 'Garnier is here to shine, and we want you to share the spotlight! Surprises await you:)',
+      'Day': 25,
+      'Month': 6
     },
     {
-      'Title': 'Event2',
-      'Desc': 'Some other description',
-      'Time': 4
+      'Title': 'Reminder',
+      'Desc': 'Garnier is here to shine, and we want you to share the spotlight! Surprises await you:)',
+      'Day': 27,
+      'Month': 6
+    },
+    {
+      'Title': 'Reminder',
+      'Desc': 'Navya Nanda is all about starting conversations that matter. Join us now to be a part of the difference!',
+      'Day': 29,
+      'Month': 6
+    },
+    {
+      'Title': 'Reminder',
+      'Desc': 'Want to be a part of something that’s both fun and responsible? Our mural speed painiting event has got you covered :)',
+      'Day': 6,
+      'Month': 7
+    },
+    {
+      'Title': 'Reminder',
+      'Desc': 'Your one stop destination for all information Malhar. The CL meet happens today!',
+      'Day': 8,
+      'Month': 7
+    },
+    {
+      'Title': 'Reminder',
+      'Desc': 'Missed a chance to attend the CL meet? No worries, we’ve got it online for those who couldn’t!',
+      'Day': 9,
+      'Month': 7
+    },
+    {
+      'Title': 'Reminder',
+      'Desc': 'Registrations for Contingents start today! We can’t wait to welcome you to the storm.',
+      'Day': 10,
+      'Month': 7
     }
   ];
